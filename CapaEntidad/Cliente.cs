838fd43c2bdce5af5fd3dc5d44dc39f1eacf0bf0
rodri-4002 <166069557+rodri-4002@users.noreply.github.com>
2024-05-16 17:07:13 -0500

    Add files via upload

---
+++ CapaEntidad/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Cliente
+    {
+        //Mapeado Objeto Relacional
+        public string CodCliente { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombres { get; set; }
+        public string Direccion { get; set; }
+    }
+}
